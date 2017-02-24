0d6db63adf29f653cc6e7361198e526b70dfc305
preeny95 <preeny@sky.com>
2017-02-24 23:28:06 +0000

    24/02
    Majority of the functions in basic functionality.

--- gui.py
+++ gui.py
@@ -8,7 +8,7 @@ class Analysis(ttk.Frame):
         ttk.Frame.__init__(self, parent, *args, **kwargs)
         self.root = parent
         self.init_gui()
-#--------------------------------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------Exit Button-----------------------------------------------------------------
 #Function for the exit button
     def on_quit(self):
         quit()
@@ -55,43 +55,124 @@ class Analysis(ttk.Frame):
 
             cur.close()
             conn.close()
-#--------------------------------------------------------------------------------------------------------------------------------------------------
-    def init_gui(self):
-        #GUI Building and Grid options.
-        self.root.title('Grooming Analysis')
-#--------------------------------------------------------------------------------------------------------------------------------------------------
+
+#------------------------------------------------------------------Text Editor---------------------------------------------------------------------
+#Return to main gui function
+    def return_main(self):
+        self.textEdit.destroy()
+
+#Save file function.
+
+    def save_feature(self):
+        file = filedialog.asksaveasfile(mode='w')
+        if file != None:
+            data = self.textEdit.get('1.0', END+'-1c')
+            file.write(data)
+            file.close()
+
+#GUI Building and Grid options.
+
+    def init_te(self):
+        import tkinter.scrolledtext as st
+        import tkinter.filedialog as filedialog
+        from tkinter.filedialog import asksaveasfilename
+        self.root.title('Word List Creator')
+        self.textEdit = st.ScrolledText(root, width=80, height=20)
+        self.textEdit.grid(column=0, row=0)
+        self.grid(column=0, row=0, sticky='nsew')
+
         #Makes it so the user cannot move the menu bar
         self.root.option_add('*tearOFF', 'FALSE')
-#--------------------------------------------------------------------------------------------------------------------------------------------------
+
         #Menubar at the top of the program
         self.menubar = tkinter.Menu(self.root)
         self.menu_file = tkinter.Menu(self.menubar)
-        self.menu_file.add_command(label='Exit', command=self.on_quit)
+        self.menu_file.add_command(label='Save', command=self.save_feature)
+        self.menu_file.add_command(label='Exit', command=self.return_main)
         self.menubar.add_cascade(menu=self.menu_file, label='File')
         self.root.config(menu=self.menubar)
-#--------------------------------------------------------------------------------------------------------------------------------------------------
+        for child in self.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+
+#----------------------------------------------------------------Word list-----------------------------------------------------------------------
+
+    def wordop(self):
+        from tkinter.filedialog import askopenfilename
+        self.wordopp = askopenfilename()
+
+
+
+#----------------------------------------------------------------Chat Logs-----------------------------------------------------------------------
+#Insert a users chatlog
+
+    def clopen(self):
+        from tkinter.filedialog import askopenfilename
+        self.chatlog = askopenfilename()
+
+    def chatanal(self):
+        import os
+        import io
+        import re
+        import sys
+        wordlist = self.wordopp
+        f = open(wordlist)
+        l = set(w.strip().lower() for w in f)
+        chatlog = self.chatlog
+        with open(chatlog) as f:
+            found = False
+            file = open("out.txt", "w")
+            for line in f:
+                line = line.lower()
+                if any(w in line for w in l):
+                    found = True
+                    file.write(line)
+            if not found:
+                print("not here")
+
+
+
+#----------------------------------------------------------------GUI Grid and Buttons-----------------------------------------------------------------------
+
+    def init_gui(self):
+        #GUI Building and Grid options.
+        self.root.title('Grooming Analysis')
+
+        #Makes it so the user cannot move the menu bar
+        self.root.option_add('*tearOFF', 'FALSE')
+
+        #Menubar at the top of the program
+        self.menubar = tkinter.Menu(self.root)
+        self.menu_file = tkinter.Menu(self.menubar)
+        self.menubar.add_cascade(menu=self.menu_file, label='File')
+        self.menu_file.add_command(label='Exit', command=self.on_quit)
+        self.root.config(menu=self.menubar)
+
         #Viber Message Extraction buttons
-        self.viber_button = ttk.Button(self, width=20, text='Insert Viber Database', command=self.viber)
-        self.viber_button.grid(column=3, row=1)
+        self.viber_button = ttk.Button(self, width=25, text='Insert Viber Database', command=self.viber)
+        self.viber_button.grid(column=2, row=1)
         self.viberanal_button = ttk.Button(self, width=25, text='Viber Database Analyse', command=self.viber_db)
-        self.viberanal_button.grid(column=4, row=1)
-#--------------------------------------------------------------------------------------------------------------------------------------------------
+        self.viberanal_button.grid(column=3, row=1)
+
         #Word list Button
-        self.words_button = ttk.Button(self, width=15, text='Words List')
+        self.words_button = ttk.Button(self, width=25, text='Insert Words List', command=self.wordop)
         self.words_button.grid(column=3, row=2)
-#--------------------------------------------------------------------------------------------------------------------------------------------------
+        self.words_button = ttk.Button(self, width=25, text='Create Word List', command=self.init_te)
+        self.words_button.grid(column=2, row=2)
+
         #Chat log button
-        self.chatlog_button = ttk.Button(self, width=15, text='Chat Log')
+        self.chatlog_button = ttk.Button(self, width=25, text='Insert Chat log', command=self.clopen)
+        self.chatlog_button.grid(column=2, row=3)
+        self.chatlog_button = ttk.Button(self, width=25, text='Analyse Chat Log', command=self.chatanal)
         self.chatlog_button.grid(column=3, row=3)
-#--------------------------------------------------------------------------------------------------------------------------------------------------
+
         #Grid Options
         self.grid(column=0, row=0, sticky='nsew')
         for child in self.winfo_children():
             child.grid_configure(padx=5, pady=5)
 
 
-
-
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#Gui loop
 if __name__ == '__main__':
     root = tkinter.Tk()
     Analysis(root)
