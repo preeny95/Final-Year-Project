00eac32969609e5c7f3aaf5974e50a2d63f44037
preeny95 <preeny@sky.com>
2017-03-31 02:00:36 +0100

    Finished Project
    Project fully finished awaiting feedback.

--- gui.py
+++ gui.py
@@ -8,12 +8,15 @@ class Analysis(ttk.Frame):
         ttk.Frame.__init__(self, parent, *args, **kwargs)
         self.root = parent
         self.init_gui()
-#-----------------------------------------------------------------------Exit Button-----------------------------------------------------------------
+        parent.minsize(width=965, height=500)
+        parent.maxsize(width=965, height=500)
+#-----------------------------------------------------------------------Exit Button----------------------------------------------------------------
 #Function for the exit button
     def on_quit(self):
         quit()
 
 #--------------------------------------------------------------VIBER DATABSASE ANALYSIS-------------------------------------------------------------
+#Adapted from Stackoverflow.com by Parfait
     def viber(self):
         from tkinter.filedialog import askopenfilename
         self.vfilename = askopenfilename()
@@ -24,15 +27,13 @@ class Analysis(ttk.Frame):
             import pandas as pd
 
             conn = sqlite3.connect(self.vfilename)
-
             cur = conn.cursor()
-            #Adapted from Stackoverflow.com by Parfait
             cur = cur.execute("""SELECT DISTINCT messages.conversation_id
                                 FROM messages
                                 INNER JOIN participants_info  ON messages.participant_id = participants_info._id
                                 WHERE messages.conversation_id IS NOT NULL;""")
 
-            query = ("""SELECT strftime('%Y-%m-%d %H:%M:%S',messages.date/1000,'unixepoch'), participants_info.number, participants_info.contact_name, messages.body AS Message_Sent, messages.conversation_id, messages.participant_id
+            query = ("""SELECT strftime('%Y-%m-%d %H:%M:%S',messages.date/1000,'unixepoch') AS Time, participants_info.number AS Number, COALESCE(participants_info.contact_name, 'Phone Analysed') AS Contact, messages.body AS Message_Sent, messages.conversation_id AS ConversationID, messages.participant_id AS ParticipantID
                         FROM messages
                         INNER JOIN
                         participants ON messages.participant_id = participants._id
@@ -57,18 +58,17 @@ class Analysis(ttk.Frame):
             conn.close()
 
 #------------------------------------------------------------------Text Editor---------------------------------------------------------------------
+#Tutorial followed at http://knowpapa.com/text-editor/
 #Return to main gui function
     def return_main(self):
-        self.textEdit.destroy()
-        #self.init_gui.destroy()
-        #root = tkinter.Tk()
-        #Analysis(root)
-        #root.mainloop()
+        self.textEdit.grid_forget()
+
+
 
 #Save file function.
 
     def save_feature(self):
-        file = filedialog.asksaveasfile(mode='w')
+        file = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
         if file != None:
             data = self.textEdit.get('1.0', END+'-1c')
             file.write(data)
@@ -77,11 +77,12 @@ class Analysis(ttk.Frame):
 #GUI Building and Grid options.
 
     def init_te(self):
+
         import tkinter.scrolledtext as st
         import tkinter.filedialog as filedialog
         from tkinter.filedialog import asksaveasfilename
         self.root.title('Word List Creator')
-        self.textEdit = st.ScrolledText(root, width=80, height=20)
+        self.textEdit = st.ScrolledText(root, width=118, height=31)
         self.textEdit.grid(column=0, row=0)
         self.grid(column=0, row=0, sticky='nsew')
 
@@ -95,44 +96,50 @@ class Analysis(ttk.Frame):
         self.menu_file.add_command(label='Exit', command=self.return_main)
         self.menubar.add_cascade(menu=self.menu_file, label='File')
         self.root.config(menu=self.menubar)
-        for child in self.winfo_children():
-            child.grid_configure(padx=5, pady=5)
+        self.grid()
+
 
 #----------------------------------------------------------------Word list-----------------------------------------------------------------------
 
     def wordop(self):
         from tkinter.filedialog import askopenfilename
-        self.wordopp = askopenfilename()
-
+        self.wordopen = askopenfilename(title="Please selet your chat log", filetypes=[("Text files","*.txt"), ("All Files","*.*")])
 
 
 #----------------------------------------------------------------Chat Logs-----------------------------------------------------------------------
 #Insert a users chatlog
-
+#Adapted from the following websites:
+#http://stackoverflow.com/questions/3964681/find-all-files-in-directory-with-extension-txt-in-python
+#http://stackoverflow.com/questions/19007383/compare-two-different-files-line-by-line-in-python
     def clopen(self):
-        from tkinter.filedialog import askopenfilename
-        self.chatlog = askopenfilename()
+        from tkinter.filedialog import askdirectory
+        self.chatopen = askdirectory(title="Select chat log directory")
 
     def chatanal(self):
+        out = filedialog.asksaveasfile(mode='w', defaultextension=".txt", filetypes=[("Text files","*.txt"), ("All Files","*.*")])
+        import fnmatch
         import os
-        import io
-        import re
         import sys
-        wordlist = self.wordopp
-        f = open(wordlist)
-        l = set(w.strip().lower() for w in f)
-        chatlog = self.chatlog
-        with open(chatlog) as f:
-            found = False
-            file = open("out.txt", "w")
-            for line in f:
-                line = line.lower()
-                if any(w in line for w in l):
-                    found = True
-                    file.write(line)
-            if not found:
-                print("not here")
-
+        path = self.chatopen
+        files = os.listdir(path)
+        paths = []
+        wordlist = self.wordopen
+        word = open(wordlist)
+        l = set(w.strip().lower() for w in word)
+        inchat = []
+        for file in files:
+            paths.append(os.path.join(path, file))
+            if fnmatch.fnmatch(file, '*.txt'):
+                with open(paths[-1]) as f:
+                    found = False
+                    for line in f:
+                        line = line.lower()
+                        if any(w in line for w in l):
+                            found = True
+                            print (line)
+                            out.write(line)
+                            if not found:
+                                print("not here")
 
 
 #----------------------------------------------------------------GUI Grid and Buttons-----------------------------------------------------------------------
@@ -151,28 +158,38 @@ class Analysis(ttk.Frame):
         self.menu_file.add_command(label='Exit', command=self.on_quit)
         self.root.config(menu=self.menubar)
 
+        #Instructions
+        frame = Frame(self, borderwidth=1, relief="solid")
+        frame.pack(side=TOP)
+        labeltext = StringVar()
+        #labeltext.set("")
+        labeltext.set("This is a python program to enable a user to analyse a Viber database and run language analysis on the chats within. \n\nCreated by Nathan Preen for my Final Year Project at Leeds Beckett University. \n\nThe buttons below are as followed.\n\n Insert Viber Database: This will open a file dialog for the user to select the viber database.\n\n Viber Database Analyse:  This will run the analysis script and output the viber chats into the root folder of the script. The chat logs will be named automatically based on the conversation id's from the database. The user will also be given a HTML and Text format. \n\n Create Word List: This will open an inbuilt text editor to allow the user to create their own words list. This will not automatically direct the program to the word list, the user must set the word list using the insert words list button. Please note to be able to understand the analysis results your first word must be 'Time'  \n\n Insert Words List: This button will allow the user to insert their precreated word list (These must be in .txt format) Please note to be able to understand the analysis results your first word must be 'Time'. \n\n Insert Chat Logs: This button will ask the user to point the program to the directory where all of the chat logs are stored.\n\n Analyse Chat log: This button will run the analysis based on the files passed to the program by the user. The user MUST have inserted a word list and chat log directory to work.")
+        self.label = Label(frame, textvariable=labeltext, width=120, height=30, wraplength=600)
+        self.label.grid(column=0, row=1, columnspan=6, rowspan=4, pady=5, padx=5)
+
         #Viber Message Extraction buttons
-        self.viber_button = ttk.Button(self, width=25, text='Insert Viber Database', command=self.viber)
-        self.viber_button.grid(column=2, row=1)
-        self.viberanal_button = ttk.Button(self, width=25, text='Viber Database Analyse', command=self.viber_db)
-        self.viberanal_button.grid(column=3, row=1)
+        bframe = Frame(self, borderwidth=1, relief="solid", bg="red")
+        bframe.pack(side=RIGHT)
+
+        self.viber_button = ttk.Button(bframe, width=25, text='Insert Viber Database', command=self.viber)
+        self.viber_button.grid(column=1, row=0,sticky='N')
+        self.viberanal_button = ttk.Button(bframe, width=25, text='Viber Database Analyse', command=self.viber_db)
+        self.viberanal_button.grid(column=2, row=0,sticky='N')
 
         #Word list Button
-        self.words_button = ttk.Button(self, width=25, text='Insert Words List', command=self.wordop)
-        self.words_button.grid(column=3, row=2)
-        self.words_button = ttk.Button(self, width=25, text='Create Word List', command=self.init_te)
-        self.words_button.grid(column=2, row=2)
+        self.words_button = ttk.Button(bframe, width=25, text='Insert Words List', command=self.wordop)
+        self.words_button.grid(column=4, row=0,sticky='N')
+        self.words_button = ttk.Button(bframe, width=25, text='Create Word List', command=self.init_te)
+        self.words_button.grid(column=3, row=0,sticky='N')
 
         #Chat log button
-        self.chatlog_button = ttk.Button(self, width=25, text='Insert Chat log', command=self.clopen)
-        self.chatlog_button.grid(column=2, row=3)
-        self.chatlog_button = ttk.Button(self, width=25, text='Analyse Chat Log', command=self.chatanal)
-        self.chatlog_button.grid(column=3, row=3)
+        self.chatlog_button = ttk.Button(bframe, width=25, text='Insert Chat log Directory', command=self.clopen)
+        self.chatlog_button.grid(column=5, row=0)
+        self.chatlog_button = ttk.Button(bframe, width=25, text='Analyse Chat Log', command=self.chatanal)
+        self.chatlog_button.grid(column=6, row=0)
 
         #Grid Options
-        self.grid(column=0, row=0, sticky='nsew')
-        for child in self.winfo_children():
-            child.grid_configure(padx=5, pady=5)
+        self.grid()
 
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------
